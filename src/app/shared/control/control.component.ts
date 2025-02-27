7ad2291590dd920e4e090b6c6714e1443d30ec23
vighnesh Kargutkar <kargutkar860@gmail.com>
2025-02-27 23:38:25 +0530

    add new control component and update styles for dashboard items and tickets

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation:ViewEncapsulation.None
+})
+export class ControlComponent {
+  @Input() label!: string;
+  
+}
