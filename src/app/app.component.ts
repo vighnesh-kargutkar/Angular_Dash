49f8522577ce76c686285f984091fab1f850dc76
vighnesh-kargutkar <kargutkar860@gmail.com>
2025-02-27 17:21:41 +0530

    updated components and added new tickets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from "./dashoard/server-status/server-status.component";
 import { TrafficComponent } from "./dashoard/traffic/traffic.component";
 import { TicketsComponent } from "./dashoard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashoard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
