03ad5d3b09aee66af41e0d552b0d19462183a583
vighnesh-kargutkar <kargutkar860@gmail.com>
2025-02-28 14:28:04 +0530

    styling chnages

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appbutton]',
+  selector: 'button[appbutton],a[appbutton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
