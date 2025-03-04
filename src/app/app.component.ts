677c29148093e675aa27b1a895a1985793ad8bef
vighnesh Kargutkar <kargutkar860@gmail.com>
2025-03-04 22:53:19 +0530

    finall

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
-
-import { HeaderComponent } from './header/header.component';
-import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
-import { TrafficComponent } from './dashboard/traffic/traffic.component';
-import { TicketsComponent } from './dashboard/tickets/tickets.component';
-import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+import { RectComponent } from './rect/rect.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    ServerStatusComponent,
-    TrafficComponent,
-    TicketsComponent,
-    DashboardItemComponent
-  ],
   templateUrl: './app.component.html',
+  imports: [RectComponent, FormsModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  rectSize = {
+    width: '100',
+    height: '100',
+  };
+}
